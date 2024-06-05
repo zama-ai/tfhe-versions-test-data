8826b5b12c8e04eb52237dea617c2b010319da48
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-06-05 18:59:01 +0200

    feat: return string errors

--- src/load.rs
+++ src/load.rs
@@ -15,12 +15,12 @@ pub fn dir_for_latest<P: AsRef<Path>>(base_dir: P) -> PathBuf {
     path
 }
 
-pub fn load_versioned<Data: DeserializeOwned, P: AsRef<Path>>(path: P) -> Result<Data, ()> {
-    let file = File::open(path).map_err(|_| ())?;
-    ciborium::de::from_reader(file).map_err(|_| ())
+pub fn load_versioned<Data: DeserializeOwned, P: AsRef<Path>>(path: P) -> Result<Data, String> {
+    let file = File::open(path).map_err(|e| format!("{}", e))?;
+    ciborium::de::from_reader(file).map_err(|e| format!("{}", e))
 }
 
-pub fn load_metadata<P: AsRef<Path>>(path: P) -> Result<Vec<TestMetadata>, ()> {
-    let serialized = fs::read_to_string(path).map_err(|_| ())?;
-    ron::from_str(&serialized).map_err(|_| ())
+pub fn load_metadata<P: AsRef<Path>>(path: P) -> Result<Vec<TestMetadata>, String> {
+    let serialized = fs::read_to_string(path).map_err(|e| format!("{}", e))?;
+    ron::from_str(&serialized).map_err(|e| format!("{}", e))
 }
