e6c8fa89ece56bc4db394eb26454d0441a7c1d03
Nicolas Sarlin <nicolas.sarlin@zama.ai>
2024-06-13 13:53:16 +0200

    feat: add test data for hl

--- src/generate.rs
+++ src/generate.rs
@@ -10,7 +10,8 @@ use tfhe_versionable::Versionize;
 
 use crate::{data_dir, dir_for_version, TestMetadata, TestParameterSet};
 
-pub const TEST_PARAMS: TestParameterSet = TestParameterSet {
+/// Valid parameter set that can be used in tfhe operations
+pub const VALID_TEST_PARAMS: TestParameterSet = TestParameterSet {
     lwe_dimension: 761,
     glwe_dimension: 1,
     polynomial_size: 2048,
@@ -25,6 +26,25 @@ pub const TEST_PARAMS: TestParameterSet = TestParameterSet {
     max_noise_level: 5,
     log2_p_fail: -40.05,
     ciphertext_modulus: (u64::MAX as u128) + 1,
+    encryption_key_choice: Cow::Borrowed("big"),
+};
+
+/// Invalid parameter set to test the limits
+pub const INVALID_TEST_PARAMS: TestParameterSet = TestParameterSet {
+    lwe_dimension: usize::MAX,
+    glwe_dimension: usize::MAX,
+    polynomial_size: usize::MAX,
+    lwe_noise_gaussian_stddev: f64::MAX,
+    glwe_noise_gaussian_stddev: f64::MAX,
+    pbs_base_log: usize::MAX,
+    pbs_level: usize::MAX,
+    ks_base_log: usize::MAX,
+    ks_level: usize::MAX,
+    message_modulus: usize::MAX,
+    carry_modulus: usize::MAX,
+    max_noise_level: usize::MAX,
+    log2_p_fail: f64::MAX,
+    ciphertext_modulus: u128::MAX,
     encryption_key_choice: Cow::Borrowed("big"),
 };
 
@@ -73,4 +93,9 @@ pub trait TfhersVersion {
     /// This should create tfhe-rs shortint types, versionize them and store them into the version specific directory.
     /// The metadata for the generated tests should be returned in the same order that the tests will be run.
     fn gen_shortint_data() -> Vec<TestMetadata>;
+
+    /// Generates data for the "high_level_api" module for this version.
+    /// This should create tfhe-rs HL types, versionize them and store them into the version specific directory.
+    /// The metadata for the generated tests should be returned in the same order that the tests will be run.
+    fn gen_hl_data() -> Vec<TestMetadata>;
 }
